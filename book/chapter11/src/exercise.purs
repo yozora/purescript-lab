bfeb0a03012d6fff11e7f191e8015e219ad4c78d
Yozora <yozora@gmail.com>
2016-05-19 08:03:46 -0400

    Writer and Reader exercises

--- book/chapter11/src/exercise.purs
+++ book/chapter11/src/exercise.purs
@@ -1,13 +1,19 @@
-module Exercise (testParens)
-       where
+module Exercise
+       ( testParens
+       , Level, Doc, line, indent, cat, render
+       , collatzLength, collatzSeq
+       ) where
 
-import Prelude (Unit, ($), (+), (-), eq)
-import Data.Maybe (Maybe(Just))
-import Data.String (split)
-import Data.Array (head, tail)
-import Data.Tuple (Tuple(Tuple))
+import Prelude (Unit, bind, return, flip, (<<<)
+               , ($), (+), (-), (*), (/), eq, (==), mod)
+import Data.Array (replicate, snoc)
+import Data.String (split, joinWith)
 import Data.Foldable (traverse_)
+import Data.Traversable (sequence)
+import Data.Tuple (fst, snd)
 import Control.Monad.State (State, execState, modify)
+import Control.Monad.Reader (Reader, ask, local, runReader)
+import Control.Monad.Writer (Writer, tell, runWriter)
 
 testParens :: String -> Boolean
 testParens str =
@@ -17,3 +23,46 @@ testParens str =
     count "(" = modify $ \x -> x + 1
     count ")" = modify $ \x -> x - 1
     count _ = modify $ \(x::Int) -> x
+
+type Level = Int
+
+type Doc = Reader Level String
+
+line :: String -> Doc
+line str = do
+  level <- ask
+  return $ joinWith "" (snoc (replicate level "  ") str)
+
+indent :: Doc -> Doc
+indent = local (\x -> x + 1)
+
+cat :: Array Doc -> Doc
+cat docs = do
+  strs <- sequence docs
+  return (joinWith "\r\n" strs)
+
+render :: Doc -> String
+render = flip runReader 0
+
+collatzLengthImpl :: Int -> Writer (Array Int) Int
+collatzLengthImpl = helper 0
+  where
+    helper :: Int -> Int -> Writer (Array Int) Int
+    helper acc 1 = do
+      tell [ 1 ]
+      return acc
+    helper acc n = do
+      tell [ n ]
+      helper (acc + 1) (collatz n)
+
+    collatz :: Int -> Int 
+    collatz n =
+      case n of
+        y | y `mod` 2 == 0 -> y / 2
+        y -> y * 3 + 1
+
+collatzLength :: Int -> Int
+collatzLength = fst <<< runWriter <<< collatzLengthImpl
+
+collatzSeq :: Int -> Array Int
+collatzSeq = snd <<< runWriter <<< collatzLengthImpl
--- book/chapter11/test/Main.purs
+++ book/chapter11/test/Main.purs
@@ -1,6 +1,6 @@
 module Test.Main where
 
-import Prelude (Unit, bind, (<>), ($))
+import Prelude (Unit, bind, ($))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
@@ -8,7 +8,8 @@ import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Console (TESTOUTPUT)
 
-import Exercise (testParens)
+import Exercise (testParens, line, indent, cat, render
+                , collatzLength, collatzSeq)
 
 main :: forall e. Eff (console :: CONSOLE,
                        testOutput :: TESTOUTPUT | e) Unit
@@ -27,5 +28,27 @@ main = do
       let t4 = "(()()"
       test t4 do
         Assert.equal false $ testParens t4
+    suite "Indent exercise" do
+      test "line" do
+        Assert.equal "str" $ render (line "str")
+      test "indent" do
+        let doc = indent $ line "str"
+        Assert.equal "  str" $ render doc
+      test "cat" do
+        Assert.equal "a\r\nb" $ render (cat [line "a", line "b"])
+      test "integration" do
+        let doc = indent $ cat
+                  [ line "line1"
+                  , line "line2"
+                  ]
+        let expected = "  line1\r\n  line2"
+        Assert.equal expected $ render doc
+    suite "Writer exercises" do
+      test "collatzLength" do
+        Assert.equal 0 $ collatzLength 1
+        Assert.equal 6 $ collatzLength 10
+      test "collatzSeq" do
+        let expected = [10, 5, 16, 8, 4, 2, 1]
+        Assert.equal expected $ collatzSeq 10
         
   log "Done."
